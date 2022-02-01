e66c2982124a78c58598823eb15f6a6483717635
tyisyustat24 <tyis.yustat24@icloud.com>
2022-02-01 09:17:53 -0800

    Did the rest of the demo'

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ console.log(spaceship.flying());
 
 document.write("<h2>", spaceship.flying(), "</h2>")
 document.write("<h2> My spaceship is ", spaceship.color, " and has a ", spaceship.room, " for style and comfort!" )
+
+function Icecream (flavor, dish, size){
+    this.flavor = flavor;
+    this.dish = dish;
+    this.size = size;
+}
+const jaidynsIcecream = new Icecream ('coffe' , 'cone', 'small');
+
+Icecream.prototype.toppings = this.toppings;
+const tyisIcecream = new Icecream ('cookie dough', 'bowl', 'medium');
+
+console.log (jaidynsIcecream);
+console.log (tyisIcecream);
